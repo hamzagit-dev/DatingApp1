fb8fae35bb6e90ff0250a7fc75709a1b2be19c16
Hamza <hamza.kennouche@gmail.com>
2022-05-10 08:07:05 -0400

    End of Section 10

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,10 +2,13 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ListsComponent } from "./lists/lists.component";
 import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { MemberListComponent } from "./members/member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { AuthGuard } from "./_guards/auth.guard";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
 import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
 import { MemberListResolver } from "./_resolvers/member-list.resolver";
 
 export const appRoutes: Routes = [
@@ -19,13 +22,20 @@ export const appRoutes: Routes = [
         path: "members",
         component: MemberListComponent,
         resolve: { users: MemberListResolver },
-        canActivate: [AuthGuard],
+        // canActivate: [AuthGuard],
       },
       {
         path: "members/:id",
         component: MemberDetailComponent,
         resolve: { user: MemberDetailResolver },
-        canActivate: [AuthGuard],
+        // canActivate: [AuthGuard],
+      },
+      {
+        path: "member/edit",
+        component: MemberEditComponent,
+        resolve: { user: MemberEditResolver },
+        canDeactivate: [PreventUnsavedChanges],
+        // canActivate: [AuthGuard],
       },
       {
         path: "messages",
